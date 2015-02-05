b3a019c129fc60e09247fb43617741d13497bf18
ta2edchimp <kontakt@andreaswindt.de>
2015-02-05 18:01:10 +0100

    DB tables

---
+++ database/002_brBattleParties.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `brBattleParties` (
+    `brBattlePartyID` int(11) NOT NULL AUTO_INCREMENT,
+    `battleReportID` int(11) DEFAULT 0 NOT NULL,
+    `brTeamName` text,
+    PRIMARY KEY (`brBattlePartyID`),
+    KEY `brBattleParties_IX_battleReport`(`battleReportID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
