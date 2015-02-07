9c260b060e8ddd8e1ea6fcb3f87285ae3774b34e
ta2edchimp <kontakt@andreaswindt.de>
2015-02-07 20:41:02 +0100

    Empty default config

---
+++ config.blueprint.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ *  Site Config
+ */
+define('BR_OWNER', '');
+
+define('BR_OWNERCORP_NAME', '');
+define('BR_OWNERCORP_ID', '');
+
+
+/*
+ *  Source Killboard to fetch from
+ */
+define('BR_FETCH_SOURCE_NAME', '');
+define('BR_FETCH_SOURCE_URL', '');
+
+
+/*
+ *  Database Config
+ */
+// The database server's host, usually 'localhost' / '127.0.0.1'
+define('DB_HOST', '');
+// The database's name
+define('DB_NAME', '');
+// Specify a user with sufficient access rights, and his password
+define('DB_USER', '');
+define('DB_PASS', '');
+
+
+/*
+ *  Styles Config
+ */
+define('BR_THEME', '');
+
+
+/*
+ *  Debug Mode
+ */
+$BR_DEBUGMODE = true;
+
+?>
