f0595f3c9d55c877823a9d90062f49c5b2fb05aa
ta2edchimp <kontakt@andreaswindt.de>
2015-02-13 22:26:17 +0100

    Layout improvements for logged in users.

--- twig.php
+++ twig.php
@@ -17,8 +17,19 @@ if (defined('BR_OWNER'))
     
 $twigEnv->addGlobal("BR_OWNERCORP_NAME", BR_OWNERCORP_NAME);
 
-$twigEnv->addGlobal("BR_USER_LOGGEDIN", User::isLoggedIn());
-$userIsAdmin = User::isAdmin();
+$userIsAdmin = false;
+if (User::isLoggedIn()) {
+	$twigEnv->addGlobal("BR_USER_LOGGEDIN", User::isLoggedIn());
+	$userInfo = User::getUserInfos();
+	if ($userInfo !== NULL) {
+		$twigEnv->addGlobal("BR_USER_NAME", $userInfo["userName"]);
+		if (!empty($userInfo["characterID"]))
+			$twigEnv->addGlobal("BR_USER_CHARACTERID", $userInfo["characterID"]);
+		$userIsAdmin = User::isAdmin();
+	}
+} else {
+	$twigEnv->addGlobal("BR_USER_LOGGEDIN", User::isLoggedIn());
+}
 $twigEnv->addGlobal("BR_USER_IS_ADMIN", $userIsAdmin);
 $twigEnv->addGlobal("BR_USER_CAN_CREATE", User::can('create'));
 $twigEnv->addGlobal("BR_USER_CAN_EDIT", $userIsAdmin);
