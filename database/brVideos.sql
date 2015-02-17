6fbfa9fe1ba6b2919ad0c2e9742839920ce84cb5
ta2edchimp <kontakt@andreaswindt.de>
2015-02-17 18:39:11 +0100

    Updated database schemas.

--- database/brVideos.sql
+++ database/brVideos.sql
@@ -1,6 +1,8 @@
+DROP TABLE IF EXISTS `brVideos`;
 CREATE TABLE `brVideos` (
   `videoID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `battleReportID` int(11) DEFAULT NULL,
   `videoUrl` text,
+  `videoPoVCombatantID` int(11) DEFAULT NULL,
   PRIMARY KEY (`videoID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
