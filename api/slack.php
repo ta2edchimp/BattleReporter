1713d2ef5ce2e487dd0e4ae6b76bc5cb29707394
ta2edchimp <kontakt@andreaswindt.de>
2015-02-27 14:22:24 +0100

    Basic Slash Command support for `Slack`.

--- api/slack.php
+++ api/slack.php
@@ -1,96 +1,190 @@
 <?php
 
-$app->status(200);
-$app->contentType('application/json');
+require_once ("$basePath/classes/API/Slack.php");
 
-if (!$app->request->isPost() || $app->request->post('token') != BR_API_SLACK_TOKEN) {
+$app->status(200);
+if ($app->request->isPost())
+	$app->contentType('application/json');
+
+$params = $app->request->post();
+if ($app->request->isGet()) {
+	$params = $app->request->get();
+}
+
+if (!$app->request->isPost() || BR_API_SLACK_ENABLED !== true || $app->request->post('token') != BR_API_SLACK_TOKEN) {
 	// Die silently
 	$app->stop();
 }
 
-$db = Db::getInstance();
 
-$latestBattle = $db->row(
-	"select br.battleReportID, br.brTitle as title, br.brStartTime as startTime, br.brEndTime as endTime, br.brPublished as published, " .
-		"br.brCreatorUserID as creatorUserID, ifnull(u.userName, '') as creatorUserName, " .
-		"br.brUniquePilotsTeamA, br.brUniquePilotsTeamB, br.brUniquePilotsTeamC, " .
-		"ifnull((select sum(c.priceTag) from brCombatants as c where c.brHidden = 0 and c.brBattlePartyID = (" .
-			"select bp.brBattlePartyID from brBattleParties as bp where bp.battleReportID = br.battleReportID and bp.brTeamName = 'teamA' limit 1" .
-		")), 0.0) as iskLostTeamA, " .
-		"ifnull((select sum(c.priceTag) from brCombatants as c where c.brHidden = 0 and c.brBattlePartyID = (" .
-			"select bp.brBattlePartyID from brBattleParties as bp where bp.battleReportID = br.battleReportID and bp.brTeamName = 'teamB' limit 1" .
-		")), 0.0) as iskLostTeamB, " .
-		"ifnull((select sum(c.priceTag) from brCombatants as c where c.brHidden = 0 and c.brBattlePartyID = (" .
-			"select bp.brBattlePartyID from brBattleParties as bp where bp.battleReportID = br.battleReportID and bp.brTeamName = 'teamC' limit 1" .
-		")), 0.0) as iskLostTeamC, " .
-		"sys.solarSystemName, " .
-		"(select count(commentID) from brComments as cm where cm.battleReportID = br.battleReportID and cm.commentDeleteTime is NULL) as commentCount, " .
-		"(select count(videoID) from brVideos as v where v.battleReportID = br.battlereportID) as footageCount " .
-	"from brBattles as br inner join mapSolarSystems as sys " .
-		"on br.solarSystemID = sys.solarSystemID " .
-		"left outer join brUsers as u on u.userID = br.brCreatorUserID " .
-	"where br.brDeleteTime is NULL and br.brPublished = 1 " .
-		($app->request->post('text') !== null && $app->request->post('text') != "" ? "and br.battleReportID = :brID " : "") .
-	"order by br.brStartTime desc " .
-	"limit 1",
-	($app->request->post('text') !== null && $app->request->post('text') != "" ? array("brID" => $app->request->post('text')) : array())
-);
+/**
 
-if ($latestBattle === FALSE) {
-	$app->stop();
-}
+	Init
 
-$scheme = $_SERVER["REQUEST_SCHEME"];
-if (empty($scheme)) {
-	$scheme		= "http";
-	if (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == 'on')
-		$scheme	= "https";
-}
-$url = $scheme . "://" . $_SERVER["HTTP_HOST"] . "/";
+**/
+$command		= "/battlereporter";
 
-$title = "Battle detected";
-if (!empty($latestBattle["title"]))
-	$title = "Battle: " . $latestBattle["title"];
+$mode			= "latest";
+$battleReportID	= -1;
 
-$payload = array(
-	"username" => (BR_OWNER !== "" ? (BR_OWNER . "'s ") : "") . "BattleReporter",
-	"icon_url" => "http://image.eveonline.com/Corporation/" . BR_OWNERCORP_ID . "_64.png",
-	"text" => "<" . $url . "show/" . $latestBattle["battleReportID"] . "|" . $title . "> in " . $latestBattle["solarSystemName"] . " on " . date("Y-m-d H:i", $latestBattle["startTime"])
-);
+$slackOpts		= array();
 
-$channel = "";
-if ($app->request->post('channel_name') !== null) {
-	$channel = "#" . $app->request->post('channel_name');
-	$payload["channel"] = $channel;
-	if ($payload["channel"] == "#privategroup") {
-		echo "Cannot post to private groups, sorry.";
+if (isset($params["channel_name"])) {
+	
+	$slackOpts["channel"] = "#" . $params['channel_name'];
+	
+	if ($slackOpts["channel"] == "#privategroup") {
+		echo "_Sorry_, I cannot post to private groups.";
 		$app->stop();
 	}
+	
 }
 
-$user = ""; // Currently unused ...
-if ($app->request->post('user_name') !== null) {
-	$user = "@" . $app->request->post('user_name');
+
+if (isset($params["user_id"]) && !empty($params["user_id"]) && isset($params["user_name"]) && !empty($params["user_name"])) {
+	$slackOpts["invUserID"]	= $params["user_id"];
+	$slackOpts["invUserName"]	= $params["user_name"];
 }
 
-try {
-	Utils::curl(
-		BR_API_SLACK_CHANNEL,
-		array(
-			"payload" => json_encode($payload)
-		),
-		array(
-			"postParams" => true
-		)
-	);
-} catch (Exception $ex) {
-	$app->log->error("Exception caught while trying to post to Slack WebHook.\n" . $ex);
-	echo "Could not post Battle to Slack" . (!empty($channel) ? ", channel #$channel" . "") . ".";
+
+if (isset($params["command"]) && !empty($params["command"])) {
+	$command	= $params["command"];
+}
+
+
+/**
+
+	Check current Command and its Argument
+
+**/
+if (isset($params['text'])) {
+	
+	if (is_int($params['text']) || intval($params['text']) > 0) {
+		
+		$mode			= 'show';
+		$battleReportID	= intval($params['text']);
+		
+	} else {
+		
+		$mode = strtolower($params['text']);
+		if (empty($mode))
+			$mode = 'latest';
+		
+		if ($mode !== 'latest' && $mode !== 'list') {
+			
+			echo "*" . (BR_OWNER !== '' ? (BR_OWNER . "'s ") : "") . "BattleReporter Help*\n\n" .
+				 "Use `$command` without any argument or together with `latest` to post the latest BattleReport.\n\n" .
+				 "Use `$command list` to list the last up to 25 BattleReports.\n\n" .
+				 "Use `$command 123` to post the BattleReport with ID `123`.\n\n" .
+				 "*BattleReporter cannot post to private groups!*";
+			
+			$app->stop();
+			
+		}
+		
+	}
+	
+}
+
+
+/**
+
+	List the last (max.: 25) published BattleReports
+
+**/
+if ($mode === 'list') {
+	
+	$result = Battle::getList(array(
+		"onlyPublished" => true,
+		"count" => 25
+	));
+	
+	if ($result === NULL || $result === FALSE) {
+		echo "_Sorry_, something went wrong.";
+		$app->stop();
+	}
+	
+	if (count($result) < 1) {
+		echo "_Sorry_, but there aren't any published BattleReports, yet.";
+		$app->stop();
+	}
+	
+	$sampleID		= 0;
+	$sampleTitle	= "";
+	
+	foreach ($result as $battle) {
+		
+		if ($sampleID == 0) {
+			$sampleID = $battle["battleReportID"];
+			$sampleTitle = (empty($battle["title"]) ? ("Battle in " . $battle["solarSystemName"]) : $battle["title"]);
+		}
+		
+		echo "*" . $battle["battleReportID"] . "* - " .
+			 (empty($battle["title"]) ? ("*Battle in " . $battle["solarSystemName"] . "*\n") : ("*" . $battle["title"] . "*\nin " . $battle["solarSystemName"])) . 
+			 " on " . date("Y-m-d H:i", $battle["startTime"]) .
+			 "\n\n";
+		
+	}
+	
+	if ($sampleID == 0) {
+		echo "*Hint:*\nType `$command 123` to post BattleReport #123 (example).";
+	} else {
+		echo "*Hint:*\nType `$command $sampleID` to post BattleReport #$sampleID\n*$sampleTitle*.";
+	}
+	
+	
+	$app->stop();
+	
+}
+
+
+/**
+
+	Show the latest or another specified BattleReport
+
+**/
+$battleToShow = null;
+
+if ($mode === 'latest') {
+	
+	$result = Battle::getList(array(
+		"onlyPublished" => true,
+		"count" => 1
+	));
+	
+	if ($result === NULL || $result === FALSE || count($result) !== 1) {
+		echo "_Sorry_, something went wrong.";
+		$app->stop();
+	}
+	
+	$battleToShow = $result[0];
+	
+	$mode			= 'show';
+	$battleReportID	= $battleToShow["battleReportID"];
+	
+}
+
+if ($mode !== 'show') {
+	echo "*Unknown command `$mode`*.";
 	$app->stop();
 }
 
+$slack = new Slack(BR_API_SLACK_CHANNEL);
+$battleToShow = $slack->postBattleWithID($battleReportID, $slackOpts);
+
+if ($battleToShow === null) {
+	echo "*Invalid or unknown BattleReport ID* `$battleReportID`.";
+	$app->stop();
+}
+
+
+/**
+
+	Send Response to user
+
+**/
 echo "Posted Battle" .
-	(!empty($latestBattle["title"]) ? (" \"" . $latestBattle["title"] . "\"") : "") .
-	" to Slack" .
-	(!empty($channel) ? ", channel #$channel" . "") .
-	".";
+	 (!empty($battleToShow["title"]) ? (" \"" . $battleToShow["title"] . "\"") : (" in " . $battleToShow["solarSystemName"])) .
+	 " to Slack" .
+	 (isset($slackOpts["channel"]) ? (", channel `" . $slackOpts["channel"] . "`") : "") .
+	 ".";
