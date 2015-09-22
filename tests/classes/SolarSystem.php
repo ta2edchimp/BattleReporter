53314c1d25062cb1001e62ae70253397afcc5cc5
ta2edchimp <kontakt@andreaswindt.de>
2015-09-22 13:40:05 +0200

    classes/SolarSystem.php fully covered by unit tests.

--- tests/classes/SolarSystem.php
+++ tests/classes/SolarSystem.php
@@ -15,12 +15,46 @@ use atoum;
 
 class SolarSystem extends atoum {
 
+	/**
+	 * @covers SolarSystem::getByName
+	 */
 	public function testGetByName () {
 		$this
 			->array(\SolarSystem::getByName('Jita'))
 			->isNotEmpty()
 			->hasKeys(array('name', 'id'))
 			->containsValues(array('Jita', 30000142));
+	}
+
+	/**
+	 * @covers SolarSystem::getByID
+	 */
+	public function testGetByID () {
+		$this
+			->string(\SolarSystem::getByID(30000142))
+			->isEqualTo('Jita');
+	}
+
+	/**
+	 * @covers SolarSystem::getAllByPartialName
+	 */
+	public function testAllGetByPartialNameStartingWith () {
+		$this
+			->array(\SolarSystem::getAllByPartialName('Jit'))
+			->isNotEmpty()
+			->contains(array('name' => 'Jita', 'id' => 30000142));
+	}
+
+	/**
+	 * @covers SolarSystem::getAllByPartialNameContaining
+	 */
+	public function testAllGetByPartialNameContaining () {
+		$this
+			->array(\SolarSystem::getAllByPartialName('it'))
+			->isNotEmpty()
+			->contains(array('name' => 'Jita', 'id' => 30000142))
+			->contains(array('name' => 'Arittant', 'id' => 30003595))
+			->contains(array('name' => 'Gonditsa', 'id' => 30005268));
 	}
 
 }
