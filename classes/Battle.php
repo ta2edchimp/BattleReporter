ee811795216b013f9b018b79c6a4dc7b363e411d
ta2edchimp <kontakt@andreaswindt.de>
2015-10-02 17:49:02 +0200

    Updated BR index to new prepopulated statistics.

--- classes/Battle.php
+++ classes/Battle.php
@@ -728,21 +728,20 @@ class Battle {
 				($onlyIDs === true ? "" : (", br.brTitle as title, br.brSummary as summary, br.brStartTime as startTime, br.brEndTime as endTime, br.brPublished as published, " .
 				"br.brCreatorUserID as creatorUserID, ifnull(u.userName, '') as creatorUserName, " .
 				"br.brUniquePilotsTeamA, br.brUniquePilotsTeamB, br.brUniquePilotsTeamC, " .
-				"ifnull((select sum(c.priceTag) from brCombatants as c where c.brHidden = 0 and c.brBattlePartyID = (" .
-					"select bp.brBattlePartyID from brBattleParties as bp where bp.battleReportID = br.battleReportID and bp.brTeamName = 'teamA' limit 1" .
-				")), 0.0) as iskLostTeamA, " .
-				"ifnull((select sum(c.priceTag) from brCombatants as c where c.brHidden = 0 and c.brBattlePartyID = (" .
-					"select bp.brBattlePartyID from brBattleParties as bp where bp.battleReportID = br.battleReportID and bp.brTeamName = 'teamB' limit 1" .
-				")), 0.0) as iskLostTeamB, " .
-				"ifnull((select sum(c.priceTag) from brCombatants as c where c.brHidden = 0 and c.brBattlePartyID = (" .
-					"select bp.brBattlePartyID from brBattleParties as bp where bp.battleReportID = br.battleReportID and bp.brTeamName = 'teamC' limit 1" .
-				")), 0.0) as iskLostTeamC, " .
+				"teamA.brIskLost as iskLostTeamA, teamB.brIskLost as iskLostTeamB, teamC.brIskLost as iskLostTeamC, " .
+				"teamA.brEfficiency as efficiency, " .
 				"sys.solarSystemName, " .
 				"(select count(commentID) from brComments as cm where cm.battleReportID = br.battleReportID and cm.commentDeleteTime is NULL) as commentCount, " .
 				"(select count(videoID) from brVideos as v where v.battleReportID = br.battlereportID) as footageCount ")) .
 			"from brBattles as br " .
 				($onlyIDs === true ? "" : ("inner join mapSolarSystems as sys " .
 				"on br.solarSystemID = sys.solarSystemID " .
+				"inner join brBattleParties as teamA " .
+					"on br.battleReportID = teamA.battleReportID and teamA.brTeamName = 'teamA' " .
+				"inner join brBattleParties as teamB " .
+					"on br.battleReportID = teamB.battleReportID and teamB.brTeamName = 'teamB' " .
+				"inner join brBattleParties as teamC " .
+					"on br.battleReportID = teamC.battleReportID and teamC.brTeamName = 'teamC' " .
 				"left outer join brUsers as u on u.userID = br.brCreatorUserID ")) .
 			"where br.brDeleteTime is NULL " .
 			($onlyPublished ? "and br.brPublished = 1 " : "") .
