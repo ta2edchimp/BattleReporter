9d8830e3873c890ec5929d2b41695266e89008cc
ta2edchimp <kontakt@andreaswindt.de>
2016-06-20 16:35:57 +0200

    Avoid `ZERO_DATE` for user session validity

--- database/brUsersSessions.sql
+++ database/brUsersSessions.sql
@@ -3,7 +3,7 @@ CREATE TABLE `brUsersSessions` (
     `userID` int(11) NOT NULL,
     `sessionHash` varchar(192) NOT NULL,
     `createdTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `validUntil` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `validUntil` timestamp NOT NULL DEFAULT '1970-01-01 00:00:01',
     `userAgent` text NOT NULL,
     `ip` varchar(16) NOT NULL,
     UNIQUE KEY `sessionHash` (`sessionHash`)
